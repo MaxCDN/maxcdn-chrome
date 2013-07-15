cde077a3633e4bcab9ea3432bd5c1f0561e782ce
jdorfman <justindorfman@gmail.com>
2013-07-14 18:44:22 -0700

    fixed bytesToSize function and removed GB from popup.html

--- script.js
+++ script.js
@@ -16,15 +16,16 @@ chrome.storage.local.get(['MaxConsumerKey', 'MaxConsumerSecret', 'MaxCompanyAlia
   oauth = new OAuth(options)
 
   function bytesToSize(bytes) {
-    var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-    if (bytes === 0) return 'n/a';
-    var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+    var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB']
+    if (bytes === 0) return 'n/a'
+    var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10)
     return (bytes / Math.pow(1024, i)).toFixed(1) + ' ' + sizes[i];
-}
+	}
 
   oauth.get('https://rws.netdna.com/jdorfman/reports/nodes.json/stats/monthly', function(response) {
     response = JSON.parse(response.text)
-    document.getElementById('size').innerHTML = response.data.summary.size
+    var rdss = response.data.summary.size
+    document.getElementById('size').innerHTML = bytesToSize(rdss);
     document.getElementById('cache-hits').innerHTML = response.data.summary.cache_hit
     document.getElementById('non-cache-hits').innerHTML = response.data.summary.noncache_hit
   })
