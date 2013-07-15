4f09e51958ade417dfd8ef75af0f844ce8afc655
jdorfman <justindorfman@gmail.com>
2013-07-14 18:07:16 -0700

    added MaxCompanyAlias to script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 var oauth, options
 
 options = {
@@ -8,12 +7,20 @@ options = {
 }
 
 /*globals chrome*/
-chrome.storage.local.get(['MaxConsumerKey', 'MaxConsumerSecret'], function(items) {
-  options.consumerKey = items.MaxConsumerKey
+chrome.storage.local.get(['MaxConsumerKey', 'MaxConsumerSecret', 'MaxCompanyAlias'], function(items) {
+  options.consumerKey    = items.MaxConsumerKey
   options.consumerSecret = items.MaxConsumerSecret
+  options.companyAlias = items.MaxCompanyAlias
 
   /*globals OAuth*/
   oauth = new OAuth(options)
+
+  function bytesToSize(bytes) {
+    var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+    if (bytes === 0) return 'n/a';
+    var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+    return (bytes / Math.pow(1024, i)).toFixed(1) + ' ' + sizes[i];
+}
 
   oauth.get('https://rws.netdna.com/jdorfman/reports/nodes.json/stats/monthly', function(response) {
     response = JSON.parse(response.text)
