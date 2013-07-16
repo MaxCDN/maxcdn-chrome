3e6485de5a9f8c35dd7f1ded66584cd339d40328
Nick Baugh <niftylettuce@gmail.com>
2013-07-16 02:22:52 -0700

    added working geo map with values

--- pops.js
+++ pops.js
@@ -1,26 +1,40 @@
- google.load('visualization', '1', {'packages': ['geochart']});
- google.setOnLoadCallback(drawMarkersMap);
 
-  function drawMarkersMap() {
+/*globals google,drawMarkersMap*/
+google.load('visualization', '1', {'packages': ['geochart']})
+
+function drawMarkersMap(cdnData) {
+
+  /*
   var data = google.visualization.arrayToDataTable([
-    ['City',   'Bandwidth', 'Hits'],
-    ['Seattle',      2761477,    1285.31],
-    ['Los Angeles',     1324110,    181.76],
-    ['Chicago',    959574,     117.27],
-    ['San Francisco',     907563,     130.17],
-    ['Dallas',   655875,     158.9],
-    ['Miami',     607906,     243.60],
-    ['Atlanta',   380181,     140.7],
-    ['Virginia',  371282,     102.41],
-    ['New York', 67370,      213.44]
-  ]);
+    [ 'City',   'Bandwidth', 'Hits'],
+    [ 'Seattle',      2761477,    1285.31],
+    [ 'Los Angeles',     1324110,    181.76],
+    [ 'Chicago',    959574,     117.27],
+    [ 'San Francisco',     907563,     130.17],
+    [ 'Dallas',   655875,     158.9],
+    [ 'Miami', 607906,     243.60],
+    [ 'Atlanta', 380181,     140.7],
+    [ 'Virginia', 371282, 102.41],
+    [ 'New York', 67370, 213.44]
+  ])
+  */
+
+  /*globals cdnData*/
+  var data = new google.visualization.DataTable()
+  data.addColumn('string', 'City')
+  data.addColumn('number', 'Cache Hits')
+  data.addColumn('number', 'Hits')
+  //data.addColumn('number', 'Non-cache Hits')
+  //data.addColumn('number', 'Size')
+  data.addRows(cdnData)
 
   var options = {
-//    region: 'US',
+    //region: 'US',
     displayMode: 'markers',
     colorAxis: {colors: ['black', 'orange']}
-  };
+  }
 
-  var chart = new google.visualization.GeoChart(document.getElementById('chart_div'));
-  chart.draw(data, options);
-};
+  var chart = new google.visualization.GeoChart(document.getElementById('chart_div'))
+  chart.draw(data, options)
+
+}
--- script.js
+++ script.js
@@ -6,20 +6,93 @@ options = {
   accessTokenUrl: 'https://rws.netdna.com/oauth/access_token'
 }
 
+var storageLocals = [
+    'MaxConsumerKey'
+  , 'MaxConsumerSecret'
+  , 'MaxCompanyAlias'
+]
+
 /*globals chrome*/
-chrome.storage.local.get(['MaxConsumerKey', 'MaxConsumerSecret', 'MaxCompanyAlias'], function(items) {
+chrome.storage.local.get(storageLocals, function(items) {
+
+  // validate all the storage locals are there and strings
+  for(var i=0; i<storageLocals.length; i++) {
+    // redirect to set the options
+    if (typeof items[storageLocals[i]] !== 'string') {
+      alert('Please enter your credentials')
+      chrome.tabs.create({ url: "options.html" })
+      window.close()
+      return
+    }
+  }
+
   options.consumerKey    = items.MaxConsumerKey
   options.consumerSecret = items.MaxConsumerSecret
-  options.companyAlias = items.MaxCompanyAlias
+  options.companyAlias   = items.MaxCompanyAlias
 
   /*globals OAuth*/
   oauth = new OAuth(options)
   var MCA = options.companyAlias
-//  oauth.get('https://rws.netdna.com/jdorfman/reports/nodes.json/stats/monthly', function(response) {
-  oauth.get('https://rws.netdna.com/' + MCA + '/reports/nodes.json/stats/monthly', function(response) {
+
+  /*globals moment*/
+  var query = {
+      date_from: moment().subtract('months', 1).format('YYYY-MM-DD')
+    , date_to: moment().format('YYYY-MM-DD')
+  }
+
+  var url = 'https://rws.netdna.com/'
+  url += MCA
+  url += '/reports/nodes.json/stats'
+  url += '?date_from=' + query.date_from
+  url += '&'
+  url += 'date_to=' + query.date_to
+
+  oauth.get(url, function(response) {
+
     response = JSON.parse(response.text)
-    document.getElementById('size').innerHTML = numeral(response.data.summary.size).format('0.000 b');
-    document.getElementById('cache-hits').innerHTML = numeral(response.data.summary.cache_hit).format('0.a');
-    document.getElementById('non-cache-hits').innerHTML = numeral(response.data.summary.noncache_hit).format('0.a')
+
+    // validate that we actually have some data
+    var summary = response.data.summary
+    if (typeof summary.cache_hit !== 'string') {
+      chrome.tabs.create({ url: "no-data.html" })
+      window.close()
+    }
+
+    /*globals numeral*/
+    document.getElementById('size').innerHTML = numeral(summary.size).format('0.000 b');
+    document.getElementById('cache-hits').innerHTML = numeral(summary.cache_hit).format('0.a');
+    document.getElementById('non-cache-hits').innerHTML = numeral(summary.noncache_hit).format('0.a')
+
+    // response.data
+    // {
+    //   stats [
+    //     {
+    //       pop_description = city
+    //       cache_hit
+    //       hit
+    //       noncache_hit
+    //       size
+    //     }
+    //   ]
+    // }
+
+    // we have to populate cdnData like [ { city: 'blah', bandwidth: 1234, hits: 1234 } ]
+    var cdnData = []
+    for(var i=0; i<response.data.stats.length; i++) {
+      var stat = response.data.stats[i]
+      var needed = [ 'cache_hit', 'hit' ] // , 'noncache_hit', 'size' ]
+      var arr = []
+      arr.push(stat.pop_description)
+      for (var n=0; n<needed.length; n++) {
+        stat[needed[n]] = parseInt(stat[needed[n]], 10)
+        arr.push(stat[needed[n]])
+      }
+      cdnData.push(arr)
+    }
+
+    /*globals drawMarkersMap*/
+    drawMarkersMap(cdnData)
+
   })
+
 })
